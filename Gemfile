fcfca2929a5a8302c45adcbbdc4d1cb68fb63813
Heber Sandoval <he3ers@gmail.com>
2017-03-23 08:30:12 -0400

    Done.

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ gem 'faraday'
 # gem 'capistrano-rails', group: :development
 
 group :development, :test do
+  gem 'dotenv-rails'
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
 end
@@ -45,4 +46,3 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
 end
-
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   get '/search', to: 'searches#search'
   post '/search', to: 'searches#foursquare'
 
+  # when you registered your app, you set your redirect URL to 'http://localhost:3000/auth'. this is where foursquare is 'sending' users after the login process. lets handle the request
+  get '/auth', to: 'session#create' # token will be accessible here
+
+  get '/friends', to: 'searches#friends'
+
 end
